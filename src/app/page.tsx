8ebb825b78c35c9f885e13cc4ee9047a28a4dbae
ArtiomB5 <artiomb5@yande.by>
2023-08-13 20:55:46 +1000

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  const drivingLicence = 'lorem';
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto cupiditate
+        aut quis iusto qui aperiam placeat enim quae possimus pariatur ea eum magni nobis! Veritatis
+        ad vero necessitatibus accusamus!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Deserunt repudiandae delectus omnis ipsum, minima quo rerum minus vero beatae, commodi
+        quidem nostrum velit quisquam dolorum, voluptatem iste quibusdam quos ad?
+      </div>
+    </main>
+  );
+}
